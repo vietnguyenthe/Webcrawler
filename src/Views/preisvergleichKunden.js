b4c14f9f08936f70853de4c704c8c2dd56d127df
viet_nguyenthe <the_viet_nguyen@yahoo.de>
2019-08-22 09:23:29 +0200

    first commit

--- src/Views/preisvergleichKunden.js
+++ src/Views/preisvergleichKunden.js
@@ -1,11 +1,21 @@
 import React, {Component} from "react"
 import { Map, GoogleApiWrapper } from 'google-maps-react';
+import "../styles/preisvergleich.css"
+
+export default GoogleApiWrapper({
+    apiKey: 'TOKEN HERE'
+})(MapContainer);
 
 export default class preisvergleichKunden extends Component {
     render() {
         return (
             <div>
             Preisvergleich
+                <Map
+                    google={this.props.google}
+                    zoom={8}
+                    initialCenter={{ lat: 47.444, lng: -122.176}}
+                />
         </div>
         )
     }
