f98db4beebe1e3b4ca83b4150725cf496f0ecd25
kazim <kazim.guevenc@gmail.com>
2019-08-22 16:45:51 +0200

    Json Objekte mit Get von Api ziehen und anzeigen lassen (durchiterieren)

--- src/Views/preisvergleichKunden.js
+++ src/Views/preisvergleichKunden.js
@@ -1,18 +1,50 @@
 import React, {Component} from "react"
 import { Map, GoogleApiWrapper } from 'google-maps-react';
 import "../styles/preisvergleich.css"
+import axios from'axios'
 
 
 export default class preisvergleichKunden extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            anbieterListe: []
+        }
+    }
+
+    // get Json Objekte voneiner Api und steck die in die state!
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                console.log(response)
+                this.setState({anbieterListe : response.data})
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
     render() {
+        const {anbieterListe } = this.state
         return (
             <div>
-            Preisvergleich
                 <Map
                     google={this.props.google}
                     zoom={8}
                     initialCenter={{ lat: 47.444, lng: -122.176}}
                 />
+
+                <br/>
+            Preisvergleich
+
+                // gezogene und in die Variable gepackte Liste durch iterieren nach ternärerem Operator
+                {
+                    anbieterListe.length ?
+                        anbieterListe.map(post => <div key={post.id}>{post.title}</div>):
+                        null
+                }
+
         </div>
         )
     }
