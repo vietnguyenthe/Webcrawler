bafbaed4bebb3510ce31efbfe0c4f553cde736f7
kazim <kazim.guevenc@gmail.com>
2019-08-22 14:59:04 +0200

    JS Objekt erstellen wenn Sumit Button geklickt wird also Anfrage geschickt wird

--- src/Views/kunden.js
+++ src/Views/kunden.js
@@ -6,21 +6,37 @@ import gastank from "../pictures/gastank.png";
 import "../styles/kunden.css";
 import InputGroup from "react-bootstrap/InputGroup";
 import Button from "react-bootstrap/Button";
-import NavLink from "react-router-dom";
 
 export default class kunden extends Component {
 
     constructor(props) {
         super(props);
 
-        this.setState = {
+        this.state = {
             behaelter: "",
             plz: "",
         };
     }
 
+    handleChange = e => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    setBehaelter(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+
+    submitHandler = e => {
+        e.preventDefault()
+        console.log(this.state)
+    }
+
 
     render() {
+        const {behaelter, plz} = this.state
         return (
             <div>
                 <Row>
@@ -32,27 +48,30 @@ export default class kunden extends Component {
                     <Col><h4 class="text-center"><br/>1. Behältergröße</h4></Col>
                     <Col/>
                 </Row>
-                <div className="selected-behltergrsse">
+
+                <div className="selected-behltergrsse" >
+                    <form onSubmit={this.submitHandler}>
+                        <div onChange={this.setBehaelter.bind(this)}>
                     <Row>
                         <Col/>
                         <Col/>
                         <Col>
                             <label>
-                                <input type="radio" name="tankgroesse" id="1.2t"/>
+                                <input type="radio" name="behaelter" value="1.2 Tonnen" id="1.2t" />
                                 <Image src={gastank} height="120" width="120"/>
                                 <br/><h6 class="text-center">1,2 Tonnen</h6>
                             </label>
                         </Col>
                         <Col>
                             <label>
-                                <input type="radio" name="tankgroesse" id="2.1t"/>
+                                <input type="radio" name="behaelter" value="2.1 Tonnen" id="2.1t" />
                                 <Image src={gastank} height="120" width="120"/>
                                 <br/><h6 className="text-center">2,1 Tonnen</h6>
                             </label>
                         </Col>
                         <Col>
                             <label>
-                                <input type="radio" name="tankgroesse" id="2.9t"/>
+                                <input type="radio" name="behaelter" value="2.9 Tonnen" id="2.9t" />
                                 <Image src={gastank} height="120" width="120"/>
                                 <br/><h6 className="text-center">2,9 Tonnen</h6>
                             </label>
@@ -60,6 +79,7 @@ export default class kunden extends Component {
                         <Col/>
                         <Col/>
                     </Row>
+                        </div>
                     <Row>
                         <Col/>
                         <Col><h4 class="text-center"><br/>2. Postleitzahl</h4></Col>
@@ -74,7 +94,7 @@ export default class kunden extends Component {
                                     <InputGroup.Text id="basic-addon1">PLZ</InputGroup.Text>
                                 </InputGroup.Prepend>
                                     <input type="text" aria-describedby="basic-addon1" name="plz"
-                                           onChange={this.handleChange}  className="form-control"/>
+                                           onChange={this.handleChange} value={plz} className="form-control"/>
                             </InputGroup>
                         </Col>
                         <Col/>
@@ -89,12 +109,13 @@ export default class kunden extends Component {
                         <Col>
                             <br/>
                             <div class="text-center">
-                                <Button type="submit" class="btn btn-primary">Anbieter finden</Button>
+                                <Button type="submit" class="btn btn-primary" >Anbieter finden</Button>
                             </div>
                         </Col>
                         <Col/>
                     </Row>
                     <Row><br/></Row>
+                    </form>
                 </div>
 
             </div>
