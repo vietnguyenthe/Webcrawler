20be9190b79ecd5f889afabad89fa76124fb0c32
kazim <kazim.guevenc@gmail.com>
2019-08-22 11:47:37 +0200

    JS View: Kontaktformular für Unternehmen Nicht schön

--- src/Views/eingabeUnternehmen.js
+++ src/Views/eingabeUnternehmen.js
@@ -1,12 +1,78 @@
 import React, {Component} from "react"
 
 
+
+
 export default class eingabeUnternehmen extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            firmenName: '',
+            firmenAdresse: '',
+            firmenPlz: '',
+            kontaktPerson: '',
+            email: '',
+            nachricht: 'Text schreiben',
+        };
+    }
+
+    mySubmitHandler = (event) => {
+        event.preventDefault();
+    }
+
+    myChangeHandler = (event) => {
+        this.setState({[event.target.name] : event.target.value})
+    }
+
     render() {
+        const {firmenName, firmenAdresse, firmenPlz, kontaktPerson, email, nachricht} = this.state
         return (
-            <div>
-                Eingabe Unternehmen
-            </div>
-        )
+
+            <form onSubmit={this.mySubmitHandler}>
+                <p>Firmenname</p>
+                <input
+                    type='text'
+                    name={firmenName}
+                    onChange={this.myChangeHandler}
+                />
+                <p>Firmenadresse</p>
+                <input
+                    type='text'
+                    name={firmenAdresse}
+                    onChange={this.myChangeHandler}
+                />
+                <p>Firmen Postleitzahl</p>
+                <input
+                    type='text'
+                    name={firmenPlz}
+                    onChange={this.myChangeHandler}
+                />
+                <p>Kontaktperson</p>
+                <input
+                    type='text'
+                    name={kontaktPerson}
+                    onChange={this.myChangeHandler}
+                />
+                <p>Email-Adresse</p>
+                <input
+                    type='text'
+                    name={email}
+                    onChange={this.myChangeHandler}
+                />
+                <p>Nachricht</p>
+                <textarea
+                    type='text'
+                    name={nachricht}
+                    onChange={this.myChangeHandler}
+                />
+                <br/>
+                <input
+                    type='submit'
+                />
+            </form>
+
+
+
+        );
     }
 }
