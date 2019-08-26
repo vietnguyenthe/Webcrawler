0aa3ab4b0084f9f0afd4774a8bc81e525653b522
Felix Weyer <f.weyer88@googlemail.com>
2019-08-26 14:48:07 +0200

    Restservice Kontaktformular erstellt

--- src/Views/eingabeUnternehmen.js
+++ src/Views/eingabeUnternehmen.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react"
 import Footer from "./footer";
+import axios from "axios";
 
 
 
@@ -11,22 +12,36 @@ export default class eingabeUnternehmen extends Component {
             firmenName: '',
             firmenAdresse: '',
             firmenPlz: '',
-            kontaktPerson: '',
-            email: '',
-            nachricht: 'Text schreiben',
+            firmenOrt: '',
+            kontaktPerson:'',
+            betreff:'',
+            email:'',
+            textNachricht:''
         };
+
+        this.mySubmitHandler = this.mySubmitHandler.bind(this)
+        this.myChangeHandler = this.myChangeHandler.bind(this)
     }
 
     mySubmitHandler = (event) => {
         event.preventDefault();
+        console.log(this.state)
+        axios.post('http://localhost:8080/preis/anfrage', this.state)
+            .then(response => {
+                console.log(response)
+            })
+
+            .catch(error => {
+                console.log(error)
+            })
+
     }
 
     myChangeHandler = (event) => {
         this.setState({[event.target.name] : event.target.value})
     }
-
     render() {
-        const {firmenName, firmenAdresse, firmenPlz,firmenOrt, kontaktPerson, email, betreff, nachricht} = this.state
+        const {firmenName, firmenAdresse, firmenPlz,firmenOrt, kontaktPerson, email, betreff, textNachricht} = this.state
         return (
 
             <div>
@@ -37,45 +52,46 @@ export default class eingabeUnternehmen extends Component {
                     <form>
                         <div>
                             <div className="form-group  col-lg-6 col-sm-12">
-                                <label htmlFor="text">Firmenname</label>
-                                <input type="text" className="form-control" id="firmenName"
+                                <label for="text">Firmenname</label>
+                                <input type="text" className="form-control" name="firmenName" value={this.props.firmenName}
                                        onChange={this.myChangeHandler}/>
                         </div>
                             <div className="form-group col-lg-6 col-sm-12 ">
                                 <label htmlFor="text">Adresse</label>
-                                <input type="text" className="form-control" id="firmenAdresse"
+                                <input type="text" className="form-control" name="firmenAdresse" value={this.props.firmenAdresse}
                                        onChange={this.myChangeHandler}/>
                             </div>
                         </div>
                         <div className="form-group col-lg-6 col-sm-12">
-                            <label htmlFor="email">PLZ</label>
-                            <input type="text" className="form-control" id="firmenPlz"
+                            <label for="email">PLZ</label>
+                            <input type="text" className="form-control" name="firmenPlz" value={this.props.firmenPlz}
                                    onChange={this.myChangeHandler}/>
                         </div>
                         <div className="form-group col-lg-6 col-sm-12">
-                            <label htmlFor="email">Ort</label>
-                            <input type="text" className="form-control" id="firmenOrt"
+                            <label for="email">Ort</label>
+                            <input type="text" className="form-control" name="firmenOrt" value={this.props.firmenOrt}
                                    onChange={this.myChangeHandler}/>
                         </div>
                         <div className="form-group col-lg-6 col-sm-12">
-                            <label htmlFor="email">Kontaktperson</label>
-                            <input type="text" className="form-control" id="kontaktPerson"
+                            <label for="email">Kontaktperson</label>
+                            <input type="text" className="form-control" name="kontaktPerson" value={this.props.kontaktPerson}
                                    onChange={this.myChangeHandler}/>
                         </div>
                         <div className="form-group col-lg-6 col-sm-12">
-                            <label htmlFor="email">Betreff</label>
-                            <input type="text" className="form-control" id="betreff"
+                            <label for="email">Betreff</label>
+                            <input type="text" className="form-control" name="betreff" value={this.props.betreff}
                                    onChange={this.myChangeHandler}/>
                         </div>
 
                         <div className="form-group col-lg-6 col-sm-12">
-                            <label htmlFor="email">Email</label>
-                            <input type="text" className="form-control" id="E-Mail"
+                            <label for="email">Email</label>
+                            <input type="text" className="form-control" name="email" value={this.props.email}
                                    onChange={this.myChangeHandler}/>
                         </div>
                         <div className="form-group col-lg-6 col-sm-12">
-                            <label htmlFor="pwd">Nachricht</label>
-                            <textarea className="form-control" id="textNachricht" rows="6"></textarea>
+                            <label for="pwd">Nachricht</label>
+                            <textarea className="form-control" name="textNachricht" value={this.props.textNachricht}
+                                   onChange={this.myChangeHandler} rows="6"></textarea>
                         </div>
                         <div className="form-group col-lg-6 col-sm-12">
                         <button type="submit" className="btn btn-primary" onClick={this.mySubmitHandler}>
