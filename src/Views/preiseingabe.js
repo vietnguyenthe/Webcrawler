d6758639eac8ebd1ea72c24fad63da46b57d686a
viet_nguyenthe <the_viet_nguyen@yahoo.de>
2019-08-28 17:55:48 +0200

    CSS

--- src/Views/preiseingabe.js
+++ src/Views/preiseingabe.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import Footer from "./footer";
 import axios from "axios";
+import Card from "react-bootstrap/Card";
 
 
 export default class Preiseingabe extends Component{
@@ -45,42 +46,49 @@ export default class Preiseingabe extends Component{
         const {preis2700Liter, preis4850Liter, preis6400Liter, postleitzahl,kennwort} = this.state
         return (
             <div>
-                <div className="container mt-3 mb-3">
-                    <h5 className="text-center"> Preiseingabe</h5>
-                    <h6 className="text-center  ">Trage die Preise für die jeweilige PLZ ein</h6>
-
-                <form>
-                        <div className="form-group col-lg-6 col-sm-12">
-                            <label for="text">Preis (ct/l) für 2700 Liter</label>
-                            <input type="number" value={this.props.preis2700Liter} class="form-control"
-                                   name="preis2700Liter" onChange={this.myChangeHandler} required="required"/>
-                        </div>
-                        <div className="form-group col-lg-6 col-sm-12">
-                            <label for="text">Preis (ct/l) für 4800 Liter</label>
-                            <input type="number" value={this.props.preis4850Liter}  className="form-control"
-                                   name="preis4850Liter" onChange={this.myChangeHandler} required="required"/>
-                    </div>
-                    <div class="form-group col-lg-6 col-sm-12">
-                        <label for="text">Preis (ct/l) für 6400 Liter</label>
-                        <input type="number" value={this.props.preis6400Liter}  className="form-control"
-                               name="preis6400Liter" onChange={this.myChangeHandler} required="required"/>
-                    </div>
-                    <div className="form-group col-lg-6 col-sm-12">
-                        <label for="text">Postleitzahl</label>
-                        <input type="number" value={this.props.postleitzahl}  className="form-control"
-                               name="postleitzahl" onChange={this.myChangeHandler} required="required"/>
-                    </div>
-                    {/*<div className="form-group col-lg-6 col-sm-12">
+                <Card className="card-central">
+                    <Card.Header><h3>Preiseingabe</h3></Card.Header>
+                    <Card.Body>
+                        <Card.Title>Trage die Preise für die jeweilige PLZ ein</Card.Title>
+                        <Card.Text>
+                        <form>
+                            <div className="form-group col-lg-6 col-sm-12">
+                                <label htmlFor="text">Preis (ct/l) für 2700 Liter</label>
+                                <input type="number" value={this.props.preis2700Liter} className="form-control"
+                                       name="preis2700Liter" onChange={this.myChangeHandler} required="required"/>
+                            </div>
+                            <div className="form-group col-lg-6 col-sm-12">
+                                <label htmlFor="text">Preis (ct/l) für 4800 Liter</label>
+                                <input type="number" value={this.props.preis4850Liter} className="form-control"
+                                       name="preis4850Liter" onChange={this.myChangeHandler} required="required"/>
+                            </div>
+                            <div class="form-group col-lg-6 col-sm-12">
+                                <label for="text">Preis (ct/l) für 6400 Liter</label>
+                                <input type="number" value={this.props.preis6400Liter}  className="form-control"
+                                       name="preis6400Liter" onChange={this.myChangeHandler} required="required"/>
+                            </div>
+                            <div className="form-group col-lg-6 col-sm-12">
+                                <label for="text">Postleitzahl</label>
+                                <input type="number" value={this.props.postleitzahl}  className="form-control"
+                                       name="postleitzahl" onChange={this.myChangeHandler} required="required"/>
+                            </div>
+                            {/*<div className="form-group col-lg-6 col-sm-12">
                         <label for="text">Kennwort</label>
                         <input type="password" value={this.props.kennwort} className="form-control"
                                name="kennwort" onChange={this.myChangeHandler}/>
                     </div>
                     */}
-                    <div className="form-group col-lg-6 col-sm-12">
-                    <button type="submit" class="btn btn-primary" onClick={this.mySubmitHandler}>Daten aktualisieren</button>
-                    </div>
-                </form>
-             </div>
+                            <div className="form-group col-lg-6 col-sm-12">
+                                <button type="submit" className="btn btn-primary" onClick={this.mySubmitHandler}>Daten
+                                    aktualisieren
+                                </button>
+                            </div>
+                        </form>
+                        </Card.Text>
+
+                    </Card.Body>
+                </Card>
+<br/>
             </div>
         );
     }
